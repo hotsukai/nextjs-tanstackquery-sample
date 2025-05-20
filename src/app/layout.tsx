0d22628ffdb1e67083a3f2ef6a1b1651fdd75f93
hotsukai <kaihotsu.kotaro@gmail.com>
2025-05-21 08:22:24 +0900

    init tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TanstackQueryProvider from "@/lib/providers/tanstack-query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <TanstackQueryProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </TanstackQueryProvider>
   );
 }
---
+++ src/lib/fetch-user.ts
@@ -0,0 +1,6 @@
+// データの取得関数
+export async function fetchUser(userId: string, baseUrl?: string) {
+  const apiBaseUrl = baseUrl || process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const res = await fetch(`${apiBaseUrl}/api/users/${userId}`);
+  return res.json();
+}
